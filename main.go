ecadd76208981e9dc76c1e32ba5e89aea20d93f1
Musonn <43937020+Musonn@users.noreply.github.com>
2026-08-28 02:52:51 +0000

    Add concurrent worker pool

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type Post struct {
@@ -13,26 +14,46 @@ type Post struct {
 	Body   string `json:"body"`
 }
 
-func main() {
-	resp, err := http.Get(
-		"https://jsonplaceholder.typicode.com/posts/1",
-	)
+func fetch(id int) {
+	resp, err := http.Get(fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%d", id))
 	if err != nil {
-		panic(err)
+		fmt.Printf("failed to fetch post %d: %v\n", id, err)
+		return
 	}
 	defer resp.Body.Close()
 
 	var post Post
-
 	if err := json.NewDecoder(resp.Body).Decode(&post); err != nil {
-		panic(err)
+		fmt.Printf("failed to decode post %d: %v\n", id, err)
+		return
 	}
 
-	fmt.Printf(
-	"ID: %d\nUser ID: %d\nTitle: %s\nBody: %s\n",
-	post.ID,
-	post.UserID,
-	post.Title,
-	post.Body,
-)
+	fmt.Printf("%d: %s\n", post.ID, post.Title)
+}
+
+func worker(jobs <-chan int) {
+	for id := range jobs {
+		fetch(id)
+	}
+}
+
+func main() {
+	jobs := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			worker(jobs)
+		}()
+	}
+
+	for id := 1; id <= 100; id++ {
+		jobs <- id
+	}
+
+	close(jobs)
+	wg.Wait()
 }
